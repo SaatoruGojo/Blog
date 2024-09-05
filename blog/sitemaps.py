cc6364dd38ae07938d56dbdfcd5ea725cf83e07c
Harsh Anand <harshanand23561@gmail.com>
2024-09-05 17:25:43 +0530

    Update gitignore

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,5 +1,7 @@
 from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
 from .models import Post
+from taggit.models import Tag
 class PostSitemap(Sitemap):
     changefreq = 'weekly'
     priority = 0.9
@@ -7,3 +9,13 @@ class PostSitemap(Sitemap):
         return Post.published.all()
     def lastmod(self, obj):
         return obj.updated
+    
+class TagSitemap(Sitemap):
+    changefreq = 'daily'
+    priority = 0.8
+
+    def items(self):
+        return Tag.objects.all()
+
+    def location(self, obj):
+        return reverse('blog:post_list_by_tag', args=[obj.slug])
